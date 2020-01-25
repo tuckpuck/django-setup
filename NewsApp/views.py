87c0ce7cf7fe0a303aec048061b6fae6b72de15d
tuckpuck <tucker.triggs@yahoo.com>
2020-01-25 13:59:17 -0700

    add home and contact page routing

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def News(request):
     return HttpResponse("<h1>This is our latest news</h1>")
+
+def Home(request):
+    return HttpResponse("<h1>Welcome to the home page</h1>")
+
+def Contact(request):
+    return HttpResponse("<h1>You can contact me on this page</h1>")
